203a3ffeb6ec605c32e551c46e29d8ff73f4b317
xYaKuBux <xcelikyakupx@gmail.com>
2021-10-19 18:53:01 +0300

    jsCamp

---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log("Merhaba Kodlama.io")
+
+
+let konutKredileri = ["A Kredi","B Kredi","C Kredi","D Kredi","E Kredi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+
+
+
+{/* <ul>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+</ul> */}
+
+console.log(konutKredileri)
